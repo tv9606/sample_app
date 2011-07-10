cb848f7722a9d5cc2115e7eafb1c330399576adf
root <root@pony.home>
2011-07-10 16:23:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e773cf325824f3166b7ba6f74d42d11cb7d1cb35851fe7591f878b6a005db35a9b38b2714737ac3efeed19ecc6aa554d41368dbfd991bb8701f6c24efab0a028'
